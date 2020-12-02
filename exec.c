e4e3953e5b806b4b1b9efb299880d8f17c942e91
Gabrielle John <gjohn010@sledge.cs.ucr.edu>
2020-12-02 00:29:29 -0800

    modified exec.c

--- exec.c
+++ exec.c
@@ -63,10 +63,11 @@ exec(char *path, char **argv)
   // Allocate two pages at the next page boundary.
   // Make the first inaccessible.  Use the second as the user stack.
   sz = PGROUNDUP(sz);
-  if((sz = allocuvm(pgdir, sz, sz + 2*PGSIZE)) == 0)
+  stackSize = KERNBASE - PGSIZE;    //starting point of the stack bc it grows backwards
+  if((sz = allocuvm(pgdir, stackSize, stackSize + 8)) == 0) //dont want to exceed the user memory
     goto bad;
   clearpteu(pgdir, (char*)(sz - 2*PGSIZE));
-  sp = sz;
+  sp = TOPSTACK;
 
   // Push argument strings, prepare rest of stack in ustack.
   for(argc = 0; argv[argc]; argc++) {
--- proc.h
+++ proc.h
@@ -49,6 +49,7 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  uint szStack;	       // size of stack memory
 };
 
 // Process memory is laid out contiguously, low addresses first:
